88e40f43657c78eec2c4759b321a9925181e1d99
FelipeEmbersisc <felipe.embersisc@gmail.com>
2021-06-09 16:24:15 -0300

    add responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,13 @@
 const imageSlider = (image) => {
-  document.querySelector(".starbucks").src = image;
+  document.querySelector('.starbucks').src = image;
 };
 
 const changeCircleColor = (color) => {
-  const circle = document.querySelector(".circle");
+  const circle = document.querySelector('.circle');
   circle.style.background = color;
+};
+
+const toggleMenu = () => {
+  let menuToggle = document.querySelector('.toggle');
+  menuToggle.classList.toggle('active');
 };
