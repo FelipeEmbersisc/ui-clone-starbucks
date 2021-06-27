ba63b898780e12166ce3c65198e54d58951e5a66
FelipeEmbersisc <felipe.embersisc@gmail.com>
2021-06-27 11:09:35 -0300

    add responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,8 @@ const changeCircleColor = (color) => {
 
 const toggleMenu = () => {
   let menuToggle = document.querySelector('.toggle');
+  let navigation = document.querySelector('.navigation');
+
   menuToggle.classList.toggle('active');
+  navigation.classList.toggle('active');
 };
