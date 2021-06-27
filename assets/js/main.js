b5bd35971ef328019376ac33a1d6f01fbb8e7e8d
FelipeEmbersisc <felipe.embersisc@gmail.com>
2021-06-27 11:37:07 -0300

    include social network in responsive menu

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,9 @@ const changeCircleColor = (color) => {
 const toggleMenu = () => {
   let menuToggle = document.querySelector('.toggle');
   let navigation = document.querySelector('.navigation');
+  let network = document.querySelector('.network');
 
   menuToggle.classList.toggle('active');
   navigation.classList.toggle('active');
+  network.classList.toggle('active');
 };
